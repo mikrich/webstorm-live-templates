8916871537f48cc39585a0c5cf5e8f180c80a244
Michael Richardson <michael@mjrichardson.com.au>
2017-03-28 09:36:59 +1100

    feat(RNStatelessFunctional): Add stateless functional component template

---
+++ ReactNative/RNStatelessFunctional.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import {
+  Text,
+  StyleSheet
+} from 'react-native'
+
+const #[[$Title$]]# = (props) => {
+  return (
+    <Text>Hi</Text>
+  )
+}
+
+#[[$Title$]]#.propTypes = {}
+
+const styles = StyleSheet.create({})
+
+export default #[[$Title$]]#
