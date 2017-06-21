3640cb18e734386d09787a1f7bf14fc36cf2d447
Michael Richardson <michael@mjrichardson.com.au>
2017-06-21 15:53:32 +1000

    feat(RNComponentShallowSpec): add template for enzyme shallow

---
+++ ReactNative/RNComponentShallowSpec.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import #[[$Title$]]#  from './#[[$Title$]]# '
+
+describe('#[[$Title$]]#', () => {
+  const wrapper = shallow(
+    <#[[$Title$]]# />
+  )
+
+  it('should render #[[$Title$]]# correctly', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+})
