e0c2f8336526290252e4c0a9aab225a929f802ab
Michael Richardson <michael@mjrichardson.com.au>
2017-07-04 09:27:04 +1000

    refactor(componentSpec): update to prompt implementation

--- ReactNative/RNComponentSpec.js
+++ ReactNative/RNComponentSpec.js
@@ -1,13 +1,21 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import #[[$Title$]]# from './#[[$Title$]]#'
+#set($componentName = "")
+#set($componentTitle = "")
+#foreach($str in $Component.split("-"))
+  #set($str= $str.substring(0,1).toUpperCase()+$str.substring(1))
+  #set($strLc = $str.substring(0,1).toLowerCase()+$str.substring(1))
+  #set($componentName = $componentName + $str)
+  #set($componentTitle = $componentTitle + $strLc + ' ')
+#end
+import React from 'react';
+import { shallow } from 'enzyme';
+import ${componentName} from './${NAME}';
 
-describe('#[[$Title$]]#', () => {
+describe('${componentName}', () => {
   const wrapper = shallow(
-    <#[[$Title$]]# />
-  )
+    <${componentName} />#[[$END$]]#
+  );
 
-  it('should render #[[$Title$]]# correctly', () => {
-    expect(wrapper).toMatchSnapshot()
-  })
-})
+  it('should render ${componentTitle}correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
--- ReactNative/RNConnectedComponentSpec.js
+++ ReactNative/RNConnectedComponentSpec.js
@@ -1,17 +1,25 @@
-import React from 'react'
-import mockStore from 'redux-mock-store'
+#set($componentName = "")
+#set($componentTitle = "")
+#foreach($str in $Component.split("-"))
+  #set($str= $str.substring(0,1).toUpperCase()+$str.substring(1))
+  #set($strLc = $str.substring(0,1).toLowerCase()+$str.substring(1))
+  #set($componentName = $componentName + $str)
+  #set($componentTitle = $componentTitle + $strLc + ' ')
+#end
+import React from 'react';
+import mockStore from 'redux-mock-store';
 
-import { shallow } from 'enzyme'
-import #[[$Title$]]# from './#[[$Title$]]#'
+import { shallow } from 'enzyme';
+import ${componentName} from './${NAME}';
 
-describe('#[[$Title$]]#', () => {
-  const initialState = {}
+describe('${componentName}', () => {
+  const initialState = {};
   const wrapper = shallow(
-    <#[[$Title$]]# />,
+    <${componentName} />#[[$END$]]#,
     { context: { store: mockStore(initialState) } }
-  )
+  );
 
-  it('should render #[[$Title$]]# correctly', () => {
-    expect(wrapper.dive().find('#[[$Title$]]#').dive()).toMatchSnapshot()
-  })
-})
+  it('should render ${componentTitle} correctly', () => {
+    expect(wrapper.dive().find('${componentName}').dive()).toMatchSnapshot();
+  });
+});
