75668620cb61cfba56e8a3fa410a0296fa3b50c1
Michael Richardson <michael@mjrichardson.com.au>
2017-07-06 08:47:37 +1000

    feat(RNComponenentSpec): Add handling for file name

--- ReactNative/RNComponentSpec.js
+++ ReactNative/RNComponentSpec.js
@@ -1,14 +1,16 @@
-#set($componentName = "")
-#set($componentTitle = "")
-#foreach($str in $Component.split("-"))
-  #set($str= $str.substring(0,1).toUpperCase()+$str.substring(1))
+#set($componentName = '')
+#set($componentTitle = '')
+#foreach($str in $Component.split('-'))
+  #set($str = $str.substring(0,1).toUpperCase()+$str.substring(1))
   #set($strLc = $str.substring(0,1).toLowerCase()+$str.substring(1))
   #set($componentName = $componentName + $str)
   #set($componentTitle = $componentTitle + $strLc + ' ')
 #end
+#set($file = "${NAME}")
+#set($fileName = $file.split('\.').get(0))
 import React from 'react';
 import { shallow } from 'enzyme';
-import ${componentName} from './${NAME}';
+import ${componentName} from './${fileName}';
 
 describe('${componentName}', () => {
   const wrapper = shallow(
