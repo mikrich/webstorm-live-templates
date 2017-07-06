6cbcaf56c5cf131774b9d2ade0ef917059be8e2c
Michael Richardson <michael@mjrichardson.com.au>
2017-07-07 08:25:14 +1000

    refactor(RNConnectedComponent): Add file name handling

--- ReactNative/RNConnectedComponentSpec.js
+++ ReactNative/RNConnectedComponentSpec.js
@@ -1,16 +1,21 @@
-#set($componentName = "")
-#set($componentTitle = "")
-#foreach($str in $Component.split("-"))
-  #set($str= $str.substring(0,1).toUpperCase()+$str.substring(1))
+#set($componentName = '')
+#set($componentTitle = '')
+#foreach($str in $Component.split('-'))
+  #set($str = $str.substring(0,1).toUpperCase()+$str.substring(1))
   #set($strLc = $str.substring(0,1).toLowerCase()+$str.substring(1))
   #set($componentName = $componentName + $str)
   #set($componentTitle = $componentTitle + $strLc + ' ')
+#end
+#set($file = "${NAME}")
+#set($fileName = $file.split('\.').get(0))
+#if ($fileName == "index")
+  #set($fileName = '')
 #end
 import React from 'react';
 import mockStore from 'redux-mock-store';
 
 import { shallow } from 'enzyme';
-import ${componentName} from './${NAME}';
+import ${componentName} from './${fileName}';
 
 describe('${componentName}', () => {
   const initialState = {};
@@ -19,7 +24,7 @@ describe('${componentName}', () => {
     { context: { store: mockStore(initialState) } }
   );
 
-  it('should render ${componentTitle} correctly', () => {
+  it('should render ${componentTitle}correctly', () => {
     expect(wrapper.dive().find('${componentName}').dive()).toMatchSnapshot();
   });
 });
